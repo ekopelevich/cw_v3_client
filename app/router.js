a0055eef50a168587dbed730d3f9fa06111e55fc
Elana Kopelevich <ekopelevich@gmail.com>
2016-08-02 11:22:32 -0600

    create and style index page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,25 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  // this.route('index');
+  this.route('login');
+  this.route('dashboard', function(){
+    this.route('profile');
+    this.route('stories');
+    this.route('contributions');
+    this.route('pending');
+    this.route('starred');
+  });
+  this.route('stories', function(){
+    this.route('new');
+    this.route('edit');
+  });
+  this.route('story', {path: 'stories/:story_id'}, function(){
+    this.route('contributions', function(){
+      this.route('new');
+      this.route('edit');
+    });
+  });
 });
 
 export default Router;
