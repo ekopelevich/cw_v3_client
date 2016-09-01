51ff93ed72195e58e5327a88101c94e9cb3fdd7e
Elana Kopelevich <ekopelevich@gmail.com>
2016-09-01 10:00:56 -0600

    clean up router with new linting rules

--- app/router.js
+++ app/router.js
@@ -1,29 +1,29 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
-  location: config.locationType
-});
+  location: config.locationType,
+})
 
 Router.map(function() {
-  this.route('login');
+  this.route('login')
   this.route('dashboard', function(){
-    this.route('profile');
-    this.route('stories');
-    this.route('contributions');
-    this.route('pending');
-    this.route('starred');
-  });
+    this.route('profile')
+    this.route('stories')
+    this.route('contributions')
+    this.route('pending')
+    this.route('starred')
+  })
   this.route('stories', function(){
-    this.route('new');
-    this.route('edit');
+    this.route('new')
+    this.route('edit')
     this.route('story', {path: 'stories/:story_id'}, function(){
       this.route('contributions', function(){
-        this.route('new');
-        this.route('edit');
-      });
-    });  
-  });
-});
+        this.route('new')
+        this.route('edit')
+      })
+    })
+  })
+})
 
-export default Router;
+export default Router
