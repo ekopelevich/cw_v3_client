7d222178fb04239d26b7ac547a80bd1d8d7f33f7
Elana Kopelevich <ekopelevich@gmail.com>
2016-09-01 09:59:49 -0600

    clean up config - reduce logging; remove semicolons

--- config/environment.js
+++ config/environment.js
@@ -10,38 +10,38 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      }
+      },
     },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
-  };
+    },
+  }
 
   if (environment === 'development') {
-    ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.apiHost = 'localhost:3000' || 'https://aqueous-island-41568.herokuapp.com';
+    ENV.APP.LOG_RESOLVER = false
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_TRANSITIONS = false
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
+    ENV.apiHost = 'localhost:3000' || 'https://cw-api.herokuapp.com'
   }
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    ENV.baseURL = '/'
+    ENV.locationType = 'none'
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = '#ember-testing'
   }
 
-  if (environment === 'production') {
+  // if (environment === 'production') {
+  //
+  // }
 
-  }
-
-  return ENV;
-};
+  return ENV
+}
